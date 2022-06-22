1be69742328d9a8c2774a3545e4231cf8d23da83
Marat <marat31bv@gmail.com>
2022-06-22 16:18:17 +0600

    Lots of code deleted, sorting section no longer prints out the sorting process, added header and GET['name'] parameter

--- testing_field.php
+++ testing_field.php
@@ -1,4 +1,6 @@
 <!DOCTYPE html>
+<!-- path for after localhost
+/www/testing_field.php?name=Marat -->
 <html lang="en">
 <head>
   <meta charset="utf-8">
@@ -8,159 +10,19 @@
 </head>
 
 <body>
-  <br>
-  <br>
-  <br>
-  <!-- Testing if it is possible to use php inside html blocks -->
-  <table border="2">
 
-    <thead>
-      <tr>
-        <th colspan="2">
-          <?php
-          echo "<h1>some random txt</h1>";
-          ?>
-        </th>
-      </tr>
-    </thead>
-
-    <tbody>
-      <tr>
-        <td>1</td>
-        <td>
-          <?php
-          echo strtolower("THIS IS PHP!!!!");
-          ?>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-
-  <!-- Trying out actual php in it's simplest forms -->
-
-  <?php
-
-  //this is a constant and it is used after the rand() section
-  const THREE_NAMES = ["Marat", "Bayel", "Iskender"];
-
-  $phrase_mike = "Mike is a capitan of KR Teyvat guild which is located in Genshin Impact fictional world";
-  echo $phrase_mike;
-
-  echo strtoupper($phrase_mike), "The length of the phrase is: ", strlen($phrase_mike), "<br>",
-  "And the first letter of the phrase is ", $phrase_mike[0], "<br>";
-
-  // flex
-  echo "Now let's make something interesting: <br>
-  1) this is our string - ", $phrase_mike, "<br>
-  2) this is version B of our sting - ", str_replace("g","jij",$phrase_mike), "<br>
-  3) this is version C of our string - ", str_ireplace("g","jij",$phrase_mike), "<br>
-  4) this is the word count of our original string - ", str_word_count($phrase_mike), "<br>
-  5) ", strrev("this is our string - " . $phrase_mike), "<br>";
-
-  //using substr
-
-  echo "You want more data? <br>
-  Fine. <br>";
-
-  echo "Mike's gf is also a ", substr($phrase_mike, 9), "<br>";
-  echo "I even know which character does your location mentioned in the string begin with. <br>
-  It is: ", strpos($phrase_mike, "Teyvat")+1, "<br>";
-
-  echo "btw, the length of the original phrase is: ".strlen($phrase_mike)." stay nice and positive UwU<br>";
-
-  echo "<br>";
-  $a = gettype($phrase_mike);
-  echo $a."<br>";
-
-  //checking the is_numeric function
-  $aNumericValue = "124";
-  $aNotNumericValue = "Guten Tag";
-
-  if (is_numeric($aNumericValue)) {
-    echo "This is a Success <br>";
-  }
-  if (is_numeric($aNotNumericValue) == false) {
-    echo "This is a second success <br>";
-  }
-
-  echo "<br>";
-
-  //trying out min() and max() functions
-  $array_with_nums = [1, 353, 6565, -214, 43, 235, -345, 452];
-  echo json_encode($array_with_nums)."<br>";
-  // echo "showing range of this array " . $array_with_nums_to_string . ": ";
-
-  if (min($array_with_nums) != 0) {
-    echo (max($array_with_nums)-min($array_with_nums)), "<br>";
-  } else {
-    echo "WRONG DATA INPUT <br>";
-  }
-  echo "<br>";
-
-  //turns out rand() function needs exaclty 2 arguments and here is an example of getting a random array element
-  echo "testing the random function: " . $array_with_nums [rand(0, count($array_with_nums))] . "<br>";
-
-  //checking the difference between (int) and round()
-  $float_num1 = 24.1;
-  $float_num2 = 0.9;
-  echo "We have these numbers: " . $float_num1 . ", " . $float_num2 . "<br>";
-  echo "Rounding them using round(): " . round($float_num1) . ", " . round($float_num2) . "<br>";
-  echo "Rounding them using (int): " . (int)$float_num1 . ", " . (int)$float_num2 . "<br>";
-
-  //using the constant
-
-  echo "The creator of this whole thing is " . THREE_NAMES[0] . "<br>";
-
-  //Testing == and ===
-
-  $three_int = 3;
-  $three_float = 3.0;
-
-  if ($three_int == $three_float) {
-    echo "the == says they're equal";
-  }
-  echo "<br>";
-
-  if ($three_int === $three_float) {
-    echo "the === says they're identical";
-  } elseif ($three_int != $three_float) {
-    echo "the !== says they're not identical";
-  }
-  echo "<br>";
-
-  //Trying the switch operator
-
-  switch ($three_int) {
-    case $three_float:
-    echo "The switch/case operator says 3 == 3.0";
-    break;
-
-    default:
-    echo "urpp2smol";
-    break;
-  }
-  echo "<br>";
-
-  //Using loops
-
-  $counting_var = 1;
-
-  while ($counting_var < 5) {
-    echo "counting_var equals to $counting_var and is lower than 5 <br>";
-    $counting_var++;
-  }
-
-  do {
-    $counting_var--;
-    echo "jij <br>";
-  } while ($counting_var > 6);
-  echo "counting_var is equal to $counting_var at the end of the loop <br>";
-
-  for ($x = 0; $x <= 100; $x+=10) {
-    echo "The number is: $x <br>";
-  }
-  echo "end of for loop <br>";
-
+    <header>
+        <?php
+        echo "Hello! <br> 
+        My name is " . $_GET['name'] . ", and this is my <u>testing field</u> for different aspects of PHP, HTML and CSS<br>
+        The file will update often and have things deleted or moving around <br>
+        <em>Might also include JS in the future, who knows?)</em>";
+         ?>
+    </header>
+<br>
+<br>
+<br>
+<?php
   // an initially-supposed-to-be-little exercise to sort an array
   $sorting_array = [1, 2, -1, -2, 3,412, -256,2.23, -32,0];
   echo "<br> Array before sorting: ";
@@ -175,8 +37,6 @@
           $sorting_array[$i+1] = $sorting_array[$i];
           $sorting_array[$i] = $data_holder;
 
-          print_r ($sorting_array);
-          echo "<br>";
         }
       }
     }
@@ -204,7 +64,7 @@
     echo "<p style=color:".strval($md_arr[$i][1]).">Name: ".strval($md_arr[$i][0]).", favorite anime: ".strval($md_arr[$i][2])."</p><br>";
   }
 
-  //an exercise from my friend
+  //an using sorting function
 
   $list_of_names = "Enter name #1: Jason; Enter name #2: Mark; Enter name #3: Alex; Enter name #4: Chris; Enter name #5: John";
   $lines = explode(";", $list_of_names);
@@ -228,7 +88,6 @@
   echo "<br>";
   echo "<br>";
   echo "<br>";
-
-  ?>
+?>
 </body>
 </html>
